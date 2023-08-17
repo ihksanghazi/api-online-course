b8ff8710fb93c8f962f8f1fedd422f8143882dd9
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-17 16:45:40 +0700

    middleware admin and teacher done

--- routers/category_router.go
+++ routers/category_router.go
@@ -14,10 +14,14 @@ func CategoryRouter() *chi.Mux {
 	categoryServices := services.NewCategoryService(databases.DB)
 	categoryControllers := controllers.NewCategoryController(categoryServices)
 
+	// guest
+	r.Get("/", categoryControllers.FindAll)
+	r.Get("/{id}", categoryControllers.FindById)
+
+	// only admin
 	r.Group(func(r chi.Router) {
 		r.Use(middlewares.TokenMiddleware)
-		r.Get("/", categoryControllers.FindAll)
-		r.Get("/{id}", categoryControllers.FindById)
+		r.Use(middlewares.OnlyAdminMiddleware)
 		r.Post("/", categoryControllers.Create)
 		r.Put("/{id}", categoryControllers.Update)
 		r.Delete("/{id}", categoryControllers.Delete)
